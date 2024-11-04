804348a33b2f769a64ee54ba28c8356c43bd28a8
enock <enockkiptoo590@gmail>
2024-11-04 23:29:46 +0300

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+
+const colorButton ;HTMLElement =document.getElementById( elementid: "color button");
+
+colorButton.addEventListener(type='click',listener:() :void=>)
+
+    colorButton.style.backgroundColor=colorButton.style.backgroundColor==="blue" ? "blue";
